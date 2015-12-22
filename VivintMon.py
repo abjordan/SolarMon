cd0a9be5546bc10a68a4515d193857b29e5fcf5c
Alex Jordan <abjordan@gmail.com>
2015-12-22 18:17:37 -0500

    Add rc script, add inverter poller

--- VivintMon.py
+++ VivintMon.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python 
+#!/usr/bin/env python 
 
 import bs4, urllib2, MySQLdb
-import time, datetime, threading
+import time, datetime, json, threading
 
 units = {
     "W": 1,
@@ -19,7 +19,7 @@ def str_to_watts(s):
     watts = number * units[unit]
     return watts
 
-def grab_reading():
+def grab_gross_reading():
     site = urllib2.urlopen("http://192.168.1.3/production?locale=en")
     page = site.read()
 
@@ -34,12 +34,55 @@ def grab_reading():
     row_w = [str_to_watts(x) for x in row]
     return row_w
 
+def grab_microinverter_stats():
+    # This is obviously site specific. To get the right URL, open the /inventory page on the enphase web console.
+    # Fire up the Chrome Dev Tools (hit F12) and select the "Network" tab. Refresh the page and look in the timeline
+    # for a GET to the datatab/inventory_dt.rb call. I chopped off a bunch of URL parameters for things like "make sortable"
+    # for each column, etc. but it doesn't seem to matter if you specify which columns you want -- you always get all 8.
+    
+    site = urllib2.urlopen("http://192.168.1.3/datatab/inventory_dt.rb?locale=en&name=PCU&sEcho=1&iColumns=8&sColumns=part_num%2Cinstalled%2Cserial_num%2Cdevice_status%2Crunning_image%2Cassem_part_num%2Cctrl_part_num%2Clast_rpt_date&iDisplayStart=0&iDisplayLength=25")
+    page = site.read()
+
+    # Need to figure out how many microinverters there are. The return from here is a JSON object:
+    #   {
+    #      'aaData'               : [ [<per inverter info>], ...],
+    #      'sEcho'                :  1,
+    #      'iTotalRecords'        : <number of inverters available>,
+    #      'iTotalDisplayRecords' : <number returned in this query>
+    #   }
+    # Let's hear it for Hungarian notation!
+
+    j = json.loads(page)
+    records = j[u'iTotalRecords']
+
+    # If you have more than 25 microinverters, request all of them
+    if len(j[u'aaData']) == records:
+        data = j
+    else:
+        full_site = urllib2.urlopen("http://192.168.1.3/datatab/inventory_dt.rb?locale=en&name=PCU&sEcho=1&iColumns=8&sColumns=part_num%2Cinstalled%2Cserial_num%2Cdevice_status%2Crunning_image%2Cassem_part_num%2Cctrl_part_num%2Clast_rpt_date&iDisplayStart=0&iDisplayLength={0}".format(records))
+        full_page = full_site.read()
+        data = json.loads(full_page)
+
+    print data
+
+    # Data is: [part_number, install_date, serial_num, status, running_image, assembly_part_number, controller_part_no, last_report]
+    # We care about serial_num, status, and last_report - the others are less interesting. It's not clear how often they update...
+    # Seems to be about every five minutes?
+    #
+    # We return: [serial_num, status, last_report]
+    rows = []
+    for inv in data[u'aaData']:
+        row = [inv[2], inv[3], inv[7]]
+        rows.append(row)
+
+    return rows
+
 def looper():
     global next_call, db, stop
 
     start_time = time.time()
     while (stop is False):
-        results = grab_reading()
+        results = grab_gross_reading()
         now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print "Got a reading at " + now + " = %f" % results[0]
 
@@ -48,6 +91,14 @@ def looper():
         cur = db.cursor()
         cur.execute(ins)
         cur.close()
+
+        inv_stats = grab_microinverter_stats()
+        ins = "INSERT INTO inverters VALUES ('{0}', '{1}', '{2}');".format(*inv_stats)
+        db.ping(True)
+        cur = db.cursor()
+        cur.execute(ins)
+        cur.close()
+
         db.commit()
 
         sleep_time = 360 - (time.time() - start_time)
@@ -66,6 +117,7 @@ if __name__=="__main__":
     looper_thread = threading.Thread(target=looper)
     looper_thread.daemon = True
     looper_thread.start()
+
     try:
         while True:
             time.sleep(5)
